5fa260c067f643fb78071bac6b48a4284bf8c3ee
igorFrotte <igorfrotte@gmail.com>
2022-06-25 00:39:07 -0300

    Metade dos Bonus

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 let nome = "";
 
-logar();
-setInterval(carregar, 3000);
+// BONUS INICIO
 
 document.querySelector("textarea").addEventListener("keypress", enviarEnter);
 
+function trocaTelaLogin(){
+    document.querySelector(".pagLogin").classList.add("escondido");
+    document.querySelector(".pagina").classList.remove("escondido");  
+}
+
+function loading(){
+    document.querySelector(".login").classList.toggle("escondido");
+    document.querySelector(".logando").classList.toggle("escondido");
+}
+
 function enviarEnter(tecla){
-    console.log(tecla);
     if(tecla.key === 'Enter'){
+        tecla.preventDefault();
         enviar();
     }
 }
 
+// BONUS FINAL
+
 function enviar(){
     const msg = document.querySelector("textarea");
-    const dados = {
-        from: nome,
-	    to: "Todos",  
-	    text: msg.value,
-	    type: "message"  // private_message
+    if(msg.value !== ""){ // "  " problem
+     const dados = {
+            from: nome,
+	        to: "Todos",  
+	        text: msg.value,
+	        type: "message"  // private_message
+        }
+        const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', dados);
+        promessa.then(msgValida);
+        promessa.catch(msgError);
+        msg.value = "";
     }
-    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', dados);
-    promessa.then(msgValida);
-    promessa.catch(msgError);
-    msg.value = "";
 }
 
 function msgValida(resposta){
@@ -35,20 +48,25 @@ function msgError(error){
 }
 
 function logar(){
-    nome = window.prompt("Qual seu nome?");
+    nome = document.querySelector(".pagLogin input").value;
     const dados = {name: nome};
     const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', dados);
     promessa.then(nomeValido);
     promessa.catch(tratarErro);
+    loading();
 }
 
 function nomeValido(resposta){
     carregar();
     setInterval(manterLogado, 5000);
+    trocaTelaLogin();
+    setInterval(carregar, 3000);
 }
 
 function tratarErro(error){
-    logar();
+    console.log(error.response.status);
+    document.querySelector(".erroLogin").innerHTML = "Nome inválido ou já está em uso";
+    loading();
 }
 
 function manterLogado(){
